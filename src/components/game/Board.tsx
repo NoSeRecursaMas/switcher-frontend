208091187a538b50cc0c68706e563129f7aebae4
Tomas Ordoñez <tomas.ordonez@mi.unc.edu.ar>
2024-09-23 17:47:02 -0300

    SW-26 Tablero creado

---
+++ src/components/game/Board.tsx
@@ -0,0 +1,96 @@
+import { useState } from 'react';
+import { Button, Center, HStack, VStack, Box } from '@chakra-ui/react';
+import Ficha1 from "/Ficha1.png";
+import Ficha2 from "/Ficha2.png";
+import Ficha3 from "/Ficha3.png";
+import Ficha4 from "/Ficha4.png";
+
+function Tile({ value }: { value: number }) {
+    
+    let img;
+    switch (value) {
+        case 1:
+            img = Ficha1;
+            break;
+        case 2:
+            img = Ficha2;
+            break;
+        case 3:
+            img = Ficha3;
+            break;
+        case 4:
+            img = Ficha4;
+            break;
+    }
+
+    // Por ahora, clickear un tile indica el valor del tile
+    const handleClick = () => {
+        console.log(value);
+    }
+
+    return (
+        <Button
+        onClick={handleClick}
+        backgroundImage={img}
+        backgroundSize="cover"
+        variant="unstyled"
+        width="80px"
+        height="80px"
+        _hover={{
+            transform: "scale(1.1)"
+        }}
+    >
+        </Button>
+    );
+}
+
+
+export default function Board({ tiles }: { tiles: number[] }) {
+    return (
+        <>
+        {/* Borde que delimita el tablero */}
+            <Box
+                border="2px"
+                borderColor="#5e666a"
+                borderRadius="10px"
+                width="auto"
+                height="auto"
+                justifyContent="center"
+                padding="20px"
+            >
+                <VStack>
+                    <HStack>
+                        {tiles.slice(0, 6).map((tile, i) => (
+                            <Tile key={i} value={tile} />
+                        ))}
+                    </HStack>
+                    <HStack>
+                        {tiles.slice(6, 12).map((tile, i) => (
+                            <Tile key={i} value={tile} />
+                        ))}
+                    </HStack>
+                    <HStack>
+                        {tiles.slice(12, 18).map((tile, i) => (
+                            <Tile key={i} value={tile} />
+                        ))}
+                    </HStack>
+                    <HStack>
+                        {tiles.slice(18, 24).map((tile, i) => (
+                            <Tile key={i} value={tile} />
+                        ))}
+                    </HStack>
+                    <HStack>
+                        {tiles.slice(24, 30).map((tile, i) => (
+                            <Tile key={i} value={tile} />
+                        ))}
+                    </HStack>
+                    <HStack>
+                        {tiles.slice(30, 36).map((tile, i) => (
+                            <Tile key={i} value={tile} />
+                        ))}
+                    </HStack>
+                </VStack>
+            </Box>
+        </>
+    )
+}
--- src/pages/game.tsx
+++ src/pages/game.tsx
@@ -1,11 +1,26 @@
 import { useParams } from 'react-router-dom';
+import { useState } from 'react';
+import { Skeleton, Box, Text, VStack, CardBody, HStack, Heading, Card, Center, Button, Flex } from "@chakra-ui/react";
+import { roomData } from '../api/room/room-types';
+import Board from '../components/game/Board';
 
 export default function Game() {
     const id = useParams().ID;
+    // const [roomInfo, setRoomInfo] = useState<roomData>;
+
+    // Tablero mockeado, randomizado con numeros del 1 al 4, hay que cambiar el estado inicial por el que se reciba ya randomizado del backend
+    const [tiles, setTiles] = useState<number[]>(Array(36).fill(0).map(() => Math.floor(Math.random() * 4) + 1));
+
+    // Orden de turnos mockeado, hay que cambiar el estado inicial por el que se reciba del backend
+    //const [turnOrder, setTurnOrder] = useState<number[]>([1, 2, 3, 4]);
 
     return (
         <>
-        <h1>Game</h1>
+        <Box bg="#2e3940">
+            <Center h="100vh">
+                <Board tiles={tiles} />
+            </Center>
+        </Box>
         </>
     )
 }
