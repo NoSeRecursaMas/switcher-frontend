da8ef79cc461613bb405e4e92e587e1226cdd016
GonzaCanavesio <gonzalo.canavesio@mi.unc.edu.ar>
2024-09-27 17:12:47 -0300

    SW-30 Modificada la arquitectura de los mocks y actualizados los endpoints

--- src/api/mock-adapter.ts
+++ src/api/mock-adapter.ts
@@ -1,6 +1,9 @@
 import axiosClient from "./http-client";
 import MockAdapter from "axios-mock-adapter";
 
-const mockAdapter = new MockAdapter(axiosClient, { delayResponse: 200 });
+const mockAdapter = new MockAdapter(axiosClient, {
+  delayResponse: 200,
+  onNoMatch: "passthrough",
+});
 
 export default mockAdapter;
--- src/api/room/room-mock.ts
+++ src/api/room/room-mock.ts
@@ -1,18 +1,112 @@
 import mockAdapter from "../mock-adapter";
 import { roomRequest } from "./room-types";
 
-const roomMock = (isTest = false) => {
-    if (import.meta.env.VITE_MOCK === "true" || isTest) {
-        mockAdapter.onPost("/rooms").reply((config) => {
-            JSON.parse(config.data as string) as roomRequest;
-
-            const mockResponse = {
-                roomID: Math.floor(Math.random() * 100),
-            };
-
-            return [201, mockResponse];
-        });
+const roomMock = () => {
+  mockAdapter.onPost("mock/rooms").reply((config) => {
+    const data = JSON.parse(config.data as string) as roomRequest;
+    const roomName = data.roomName;
+    if (roomName === "error") {
+      return [400, { detail: "Ejemplo de error en el backend" }];
     }
+    if (roomName === "id_invalida") {
+      return [404, { detail: "No existe jugador con esa ID" }];
+    }
+    const mockResponse = {
+      roomID: Math.floor(Math.random() * 100),
+    };
+
+    return [201, mockResponse];
+  });
+
+  mockAdapter.onGet("mock/rooms").reply(() => {
+    const mockResponse = [
+      {
+        roomID: 1,
+        roomName: "Sala 1",
+        minPlayers: 2,
+        maxPlayers: 4,
+        currentPlayers: 2,
+        started: false,
+        private: true,
+      },
+      {
+        roomID: 2,
+        roomName: "Sala 2",
+        minPlayers: 3,
+        maxPlayers: 4,
+        currentPlayers: 3,
+        started: false,
+        private: false,
+      },
+      {
+        roomID: 3,
+        roomName: "Somebody once told me",
+        minPlayers: 2,
+        maxPlayers: 2,
+        currentPlayers: 2,
+        started: false,
+        private: false,
+      },
+      {
+        roomID: 4,
+        roomName: "the world is gonna roll me",
+        minPlayers: 2,
+        maxPlayers: 4,
+        currentPlayers: 2,
+        started: false,
+        private: false,
+      },
+      {
+        roomID: 5,
+        roomName: ".",
+        minPlayers: 3,
+        maxPlayers: 4,
+        currentPlayers: 3,
+        started: false,
+        private: false,
+      },
+      {
+        roomID: 6,
+        roomName: "EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE",
+        minPlayers: 2,
+        maxPlayers: 4,
+        currentPlayers: 2,
+        started: false,
+        private: false,
+      },
+      {
+        roomID: 7,
+        roomName: "SALA EMPEZADA",
+        minPlayers: 2,
+        maxPlayers: 4,
+        currentPlayers: 2,
+        started: true,
+        private: false,
+      },
+      {
+        roomID: 8,
+        roomName: "Sala llena",
+        minPlayers: 3,
+        maxPlayers: 4,
+        currentPlayers: 4,
+        started: false,
+        private: false,
+      },
+      {
+        roomID: 9,
+        roomName: "OwO",
+        minPlayers: 2,
+        maxPlayers: 3,
+        currentPlayers: 2,
+        started: false,
+        private: false,
+      },
+    ];
+
+    // return [200, []]; // Testear caso de que no haya rooms
+    // return [400, { detail: "Ejemplo de error en el backend" }]; // Testear caso de error en el backend
+    return [200, mockResponse];
+  });
 };
 
 export default roomMock;
