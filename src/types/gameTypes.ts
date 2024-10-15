be79ac9d599982c7d01ce56bbd63e4a2800a6709
GonzaCanavesio <gonzalo.canavesio@mi.unc.edu.ar>
2024-10-15 19:45:36 -0300

    SW-44 Borrados tipos innecesarios

--- src/types/gameTypes.ts
+++ src/types/gameTypes.ts
@@ -1,46 +1,46 @@
 enum Color {
-  R = "R",
-  G = "G",
-  B = "B",
-  Y = "Y",
+  R = 'R',
+  G = 'G',
+  B = 'B',
+  Y = 'Y',
 }
 
 enum Movement {
-  mov1 = "mov01",
-  mov2 = "mov02",
-  mov3 = "mov03",
-  mov4 = "mov04",
-  mov5 = "mov05",
-  mov6 = "mov06",
-  mov7 = "mov07",
+  mov1 = 'mov01',
+  mov2 = 'mov02',
+  mov3 = 'mov03',
+  mov4 = 'mov04',
+  mov5 = 'mov05',
+  mov6 = 'mov06',
+  mov7 = 'mov07',
 }
 
 enum Figure {
-  fig01 = "fig01",
-  fig02 = "fig02",
-  fig03 = "fig03",
-  fig04 = "fig04",
-  fig05 = "fig05",
-  fig06 = "fig06",
-  fig07 = "fig07",
-  fig08 = "fig08",
-  fig09 = "fig09",
-  fig10 = "fig10",
-  fig11 = "fig11",
-  fig12 = "fig12",
-  fig13 = "fig13",
-  fig14 = "fig14",
-  fig15 = "fig15",
-  fig16 = "fig16",
-  fig17 = "fig17",
-  fig18 = "fig18",
-  fige01 = "fige01",
-  fige02 = "fige02",
-  fige03 = "fige03",
-  fige04 = "fige04",
-  fige05 = "fige05",
-  fige06 = "fige06",
-  fige07 = "fige07",
+  fig01 = 'fig01',
+  fig02 = 'fig02',
+  fig03 = 'fig03',
+  fig04 = 'fig04',
+  fig05 = 'fig05',
+  fig06 = 'fig06',
+  fig07 = 'fig07',
+  fig08 = 'fig08',
+  fig09 = 'fig09',
+  fig10 = 'fig10',
+  fig11 = 'fig11',
+  fig12 = 'fig12',
+  fig13 = 'fig13',
+  fig14 = 'fig14',
+  fig15 = 'fig15',
+  fig16 = 'fig16',
+  fig17 = 'fig17',
+  fig18 = 'fig18',
+  fige01 = 'fige01',
+  fige02 = 'fige02',
+  fige03 = 'fige03',
+  fige04 = 'fige04',
+  fige05 = 'fige05',
+  fige06 = 'fige06',
+  fige07 = 'fige07',
 }
 
 interface Tile {
@@ -70,18 +70,10 @@ interface MovementCard {
   isUsed: boolean; // Usada en el turno actual
 }
 
-interface LocalMovementCard extends MovementCard {
-  isSelected: boolean;
-}
-
 interface FigureCard {
   type: Figure;
   cardID: number;
   isBlocked: boolean;
-}
-
-interface LocalFigureCard extends FigureCard {
-  isSelected: boolean;
 }
 
 interface PlayerInGame {
@@ -103,7 +95,6 @@ interface Game {
   players: PlayerInGame[];
 }
 
-
 interface GameID {
   gameID: number;
 }
@@ -122,12 +113,12 @@ interface PlayFigureCardRequest {
 }
 
 interface GameStatusMessage {
-  type: "status";
+  type: 'status';
   payload: Game;
 }
 
 interface GameEndMessage {
-  type: "end";
+  type: 'end';
   payload: {
     winnerID: number;
     username: string;
@@ -146,9 +137,7 @@ export type {
   ExtendedTile,
   CoordsTile,
   MovementCard,
-  LocalMovementCard,
   FigureCard,
-  LocalFigureCard,
   PlayerInGame,
 };
 export { Color, Movement, Figure };
